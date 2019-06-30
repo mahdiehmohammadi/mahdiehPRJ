9d7f6acdaa74318447ab44fa72e8dd318756b303
mahdiehmohammadi <mahmohammadi58@gmail.com>
2019-06-30 17:07:20 +0430

    ettefagh

---
+++ ourput.py
@@ -0,0 +1,9 @@
+def duration_data(fname):
+    data[] = open(fname).read().split()
+    duration[] = 0
+    for i in range(len(data + 1)):
+        duration[] = data [i+1] - data [i]
+
+    return duration []
+
+
---
+++ question.py
@@ -0,0 +1,40 @@
+def duration_data(fname):
+    data = open(fname).read().split()
+    duration = []
+    for i in range(len(data + 1)):
+        duration[i] = data[i + 1] - data[i]
+    return duration[i]
+
+
+def describe (duration_data):
+    describe  = []
+    t1 = 200
+    t2 =300
+    for i in range (len(duration_data [i])):
+        if duration[i] < t1 :
+            describe[i]= S
+        if t1 < duration[i] < t1 :
+            Describe [i] = M
+        if duration[i] > t2 :
+            Describe [i] = M
+
+    return describe[i]
+
+
+
+describe = describe [i].replace (',' , ' ')
+des =  str(describe).join( , [''])
+
+
+
+
+def ettefagh (des) :
+    count =  0
+
+    str(des).find(L, LSL, L[, L]])
+
+
+
+
+
+
